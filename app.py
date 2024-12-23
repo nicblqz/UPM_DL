0b4c637b5e77c7cc46d4a08352ac88550381fec3
nblqz <nicolas.blanquez-yeste@ulb.be>
2024-12-23 23:04:44 +0100

    80 epochs

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ transform = transforms.Compose([
 def load_model():
     checkpoint = torch.load("fruit_classifier.pth", map_location=device)
     num_classes = checkpoint['fc.weight'].shape[0]
-    model = models.resnet18(pretrained=True)
+    model = models.resnet18(pretrained=False)
     model.fc = nn.Linear(model.fc.in_features, num_classes)  
     model.load_state_dict(checkpoint)
     model = model.to(device)
